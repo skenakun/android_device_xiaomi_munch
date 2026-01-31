e9d04183b8c174d8aa218c42556b34fb5b3b9491
HDzungx <hdzungx@gmail.com>
2026-02-01 01:17:09 +0700

    munch: parts: Add AutoHBM qs tile
    Signed-off-by: HDzungx <hdzungx@gmail.com>

---
+++ parts/src/org/lineageos/settings/hbm/AutoHBMTileService.java
@@ -0,0 +1,76 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package org.lineageos.settings.hbm;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.R;
+import org.lineageos.settings.utils.FileUtils;
+
+public class AutoHBMTileService extends TileService {
+
+    private static final String AUTO_HBM_KEY = "auto_hbm";
+
+    private void updateUI(boolean enabled) {
+        final Tile tile = getQsTile();
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        updateUI(sharedPrefs.getBoolean(AUTO_HBM_KEY, false));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+
+        final boolean enabled =
+                !sharedPrefs.getBoolean(AUTO_HBM_KEY, false);
+
+        sharedPrefs.edit().putBoolean(AUTO_HBM_KEY, enabled).apply();
+        FileUtils.enableService(this);
+        updateUI(enabled);
+    }
+}
